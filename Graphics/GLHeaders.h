132a4b9542ed188e3661cb517cc22f52aeb90c36
FredieJackson <tekaeff@yandex.ru>
2016-11-23 13:53:50 +0300

    Update GLHeaders.h

--- Graphics/GLHeaders.h
+++ Graphics/GLHeaders.h
@@ -1,6 +1,6 @@
 #pragma once
 #include "..\Includes\glew\glew.h"
-#include <gl\GL.h>
+#include "..\Includes\GL\gl.h"
 #include "..\Includes\glfw3\glfw3.h"
 #include "..\Includes\glm\glm.hpp"
 #include "..\Includes\glm\gtx\transform.hpp"
